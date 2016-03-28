6c6c9699186a30f564cb3178e278a93981542991
Parker Donat <parkerdonat@gmail.com>
2016-03-28 09:30:56 -0600

    Views Set Up

---
+++ RedditChallenge/NetworkController.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkController.swift
+//  RedditChallenge
+//
+//  Created by Parker Donat on 3/28/16.
+//  Copyright © 2016 Parker Donat. All rights reserved.
+//
+
+import Foundation
--- RedditChallenge/ViewController.swift
+++ RedditChallenge/ViewController.swift
@@ -21,16 +21,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func submitButtonPressed(sender: AnyObject) {
+        
+        
     }
-
 
 }
 
